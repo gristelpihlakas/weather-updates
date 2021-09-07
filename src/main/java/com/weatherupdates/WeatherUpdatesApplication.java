03540048a9991896ec4dbf3423d3503a765a6050
gristelpihlakas <gristelpihlakas@gmail.com>
2021-09-07 22:37:18 +0300

    initial commit

---
+++ src/main/java/com/weatherupdates/WeatherUpdatesApplication.java
@@ -0,0 +1,13 @@
+package com.weatherupdates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherUpdatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherUpdatesApplication.class, args);
+    }
+
+}
